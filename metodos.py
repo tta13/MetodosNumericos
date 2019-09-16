cc27d20d664c66a1cd78dcd0f955547b40cedce3
tta13 <54077178+tta13@users.noreply.github.com>
2019-09-16 00:18:49 -0300

    Iniciando implementação de Adams-Moulton

--- metodos.py
+++ metodos.py
@@ -245,6 +245,197 @@ def metodo_adams_bashforth(y0, t0, h, n, f_string, ordem, y_list, saida):
 			k1 = fy.subs(t, t0)
 			print(j, y0, file = saida) 
 
+def metodo_adams_moulton(y0, t0, h, n, f_string, ordem, y_list, saida):
+	y, t = sympy.symbols("y t")
+	f = sympy.sympify(f_string)	
+	if(ordem == 1):
+		y0 = y0
+		t0 = t0
+		for j in range(0, n+1):
+			print(j, y0, file = saida)
+			t0 += h
+			func = f.subs(t, t0)
+			k1 = sympy.solveset(y0 + func*h - y, y)
+			y0 = k1.args[0]
+	elif(ordem == 2):
+		y0 = y_list[0]
+		t0 += h
+		fy = f.subs(y, y_list[0])
+		k1 = fy.subs(t, t0)
+		#implicito
+		k2 = f.subs(t, t0+h)
+		for j in range(1, n+1):
+			solution = y0 + (h/2)*(k1 + k2) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, t0)
+			k2 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+	elif(ordem == 3):
+		y0 = y_list[1]
+		t0 += h
+		fy = f.subs(y, y0)
+		k1 = fy.subs(t0)
+		fy2 = f.subs(y, y_list[0])
+		k2 = fy2.subs(t, t0 - h)
+		#implicito
+		k3 = f.subs(t, t0+h)
+		for j in range(2, n+1):
+			solution = y0 + (h/12)*(5*k3 + 8*k1 -k2) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			k2 = k1
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, t0)
+			k3 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+	elif(ordem == 4):
+		y0 = y_list[2]
+		t0 += 2*h
+		fy = f.subs(y, y0)
+		k1 = fy.subs(t, (t0))
+		fy2 = f.subs(y, y_list[1])
+		k2 = fy2.subs(t, t0 - h)
+		fy3 = f.subs(y, y_list[0])
+		k3 = fy3.subs(t, t0 - 2*h)
+		#implicito
+		k4 = f.subs(t, t0+h)
+		for j in range(3, n+1):
+			solution = y0 + (h/24)*(9*k4 + 19*k1 - 5*k2 + k3) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			k3 = k2
+			k2 = k1
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, t0)
+			k4 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+	elif(ordem == 5):
+		y0 = y_list[3]
+		t0 += 3*h
+		fy = f.subs(y, y0)
+		k1 = fy.subs(t, (t0))
+		fy2 = f.subs(y, y_list[2])
+		k2 = fy2.subs(t, t0 - h)
+		fy3 = f.subs(y, y_list[1])
+		k3 = fy3.subs(t, t0 - 2*h)
+		fy4 = f.subs(y, y_list[0])
+		k4 = fy4.subs(t, (t0 - 3*h))
+		#implicito
+		k5 = f.subs(t, t0+h)
+		for j in range(4, n+1):
+			solution = y0 + (h/720)*(251*k5 + 646*k1 - 264*k2 + 106*k3 - 19*k4) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			k4 = k3
+			k3 = k2
+			k2 = k1
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, (t0))
+			k5 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+	elif(ordem == 6):
+		y0 = y_list[4]
+		t0 += 4*h
+		fy = f.subs(y, y0)
+		k1 = fy.subs(t, (t0))
+		fy2 = f.subs(y, y_list[3])
+		k2 = fy2.subs(t, t0 - h)
+		fy3 = f.subs(y, y_list[2])
+		k3 = fy3.subs(t, t0 - 2*h)
+		fy4 = f.subs(y, y_list[1])
+		k4 = fy4.subs(t, (t0 - 3*h))
+		fy5 = f.subs(y, y_list[0])
+		k5 = fy5.subs(t, t0 - 4*h)
+		#implicito
+		k6 = f.subs(t, t0+h)
+		for j in range(5, n+1):
+			solution = y0 + (h/1440)*(475*k6 + 1427*k1 - 798*k2 + 482*k3 - 173*k4
+				+ 27*k5) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			k5 = k4
+			k4 = k3
+			k3 = k2
+			k2 = k1
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, (t0))
+			k6 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+	elif(ordem == 7):
+		y0 = y_list[5]
+		t0 += 5*h
+		fy = f.subs(y, y0)
+		k1 = fy.subs(t, (t0))
+		fy2 = f.subs(y, y_list[4])
+		k2 = fy2.subs(t, t0 - h)
+		fy3 = f.subs(y, y_list[3])
+		k3 = fy3.subs(t, t0 - 2*h)
+		fy4 = f.subs(y, y_list[2])
+		k4 = fy4.subs(t, (t0 - 3*h))
+		fy5 = f.subs(y, y_list[1])
+		k5 = fy5.subs(t, t0 - 4*h)
+		fy6 = f.subs(y, y_list[0])
+		k6 = fy6.subs(t, t0 - 5*h)
+		#implicito
+		k7 = f.subs(t, t0+h)
+		for j in range(6, n+1):
+			solution = y0 + (h/60480)*(19087*k7 + 65112*k1 - 46461*k2 + 35704*k3 - 20211*k4
+				+ 6312*k5 - 863*k6) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			k6 = k5
+			k5 = k4
+			k4 = k3
+			k3 = k2
+			k2 = k1
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, (t0))
+			k7 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+	elif(ordem == 8):
+		y0 = y_list[6]
+		t0 += 6*h
+		fy = f.subs(y, y0)
+		k1 = fy.subs(t, (t0))
+		fy2 = f.subs(y, y_list[5])
+		k2 = fy2.subs(t, t0 - h)
+		fy3 = f.subs(y, y_list[4])
+		k3 = fy3.subs(t, t0 - 2*h)
+		fy4 = f.subs(y, y_list[3])
+		k4 = fy4.subs(t, (t0 - 3*h))
+		fy5 = f.subs(y, y_list[2])
+		k5 = fy5.subs(t, t0 - 4*h)
+		fy6 = f.subs(y, y_list[1])
+		k6 = fy6.subs(t, t0 - 5*h)
+		fy7 = f.subs(y, y_list[0])
+		k7 = fy7.subs(t, t0-6*h) 
+		#implicito
+		k8 = f.subs(t, t0+h)
+		for j in range(7, n+1):
+			solution = y0 + (h/120960)*(36799*k8 + 139849*k1 - 121797*k2 + 123133*k3 - 88547*k4
+				+ 41499*k5 - 11351*k6 + 1375*k7) - y
+			solution = sympy.solveset(solution, y)
+			y0 = solution.args[0]
+			t0 += h
+			k7 = k6
+			k6 = k5
+			k5 = k4
+			k4 = k3
+			k3 = k2
+			k2 = k1
+			fy = f.subs(y, y0)
+			k1 = fy.subs(t, (t0))
+			k8 = f.subs(t, t0+h)
+			print(j, y0, file = saida)
+
 entrada = open("entrada.txt")
 saida = open("saida.txt", "w")
 lines = entrada.readlines()
@@ -272,11 +463,12 @@ for i in lines:
 		saida.write("\n")
 	elif(string_parts[0] == "adam_bashforth"):
 		print("Metodo de Adams-Bashforth", file = saida)
-		print("y( ", t0, " ) = ", y0, file = saida)
-		print("h = ", h, file = saida)
+		print("y( ", float(string_parts[-5]), " ) = ", string_parts[1], file = saida)
+		print("h = ", string_parts[-4], file = saida)
 		y_list = []
 		for j in range(1, int(string_parts[-1])+1):
 			y_list.append(float(string_parts[j]))
+			print(j, string_parts[j], file = saida)
 		metodo_adams_bashforth(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
 			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
 		saida.write("\n")
@@ -312,5 +504,50 @@ for i in lines:
 		metodo_adams_bashforth(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
 			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
 		saida.write("\n")
+	elif(string_parts[0] == "adam_multon"):
+		print("Metodo de Adams-Moulton", file = saida)
+		print("y( ", float(string_parts[-5]), " ) = ", string_parts[1], file = saida)
+		print("h = ", string_parts[-4], file = saida)
+		y_list = []
+		for j in range(1, int(string_parts[-1])):
+			y_list.append(float(string_parts[j]))
+			print(j, string_parts[j], file = saida)
+		metodo_adams_moulton(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
+			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
+		saida.write("\n")
+	elif(string_parts[0] == "adam_multon_by_euler"):
+		print("Metodo Adams-Moulton por Euler", file = saida)
+		y_list = []
+		y_list = metodo_euler(float(string_parts[1]), float(string_parts[2]), float(string_parts[3]), 
+			int(string_parts[-1]) - 2, string_parts[-2], saida)
+		metodo_adams_moulton(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
+			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
+		saida.write("\n")
+	elif(string_parts[0] == "adam_multon_by_euler_inverso"):
+		print("Metodo Adams-Moulton por Euler Inverso", file = saida)
+		y_list = []
+		y_list = metodo_euler_inverso(float(string_parts[1]), float(string_parts[2]), float(string_parts[3]), 
+			int(string_parts[-1]) - 2, string_parts[-2], saida)
+		metodo_adams_moulton(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
+			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
+		saida.write("\n")
+	elif(string_parts[0] == "adam_multon_by_euler_aprimorado"):
+		print("Metodo Adams-Moulton por Euler Aprimorado", file = saida)
+		y_list = []
+		y_list = metodo_euler_aprimorado(float(string_parts[1]), float(string_parts[2]), float(string_parts[3]), 
+			int(string_parts[-1]) - 2, string_parts[-2], saida)
+		metodo_adams_moulton(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
+			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
+		saida.write("\n")
+	elif(string_parts[0] == "adam_multon_by_runge_kutta"):
+		print("Metodo Adams-Moulton por Runge-Kutta (ordem = ", string_parts[-1], ")", file = saida)
+		y_list = []
+		y_list = metodo_runge_kutta(float(string_parts[1]), float(string_parts[2]), float(string_parts[3]), 
+			int(string_parts[-1]) - 2, string_parts[-2], saida)
+		metodo_adams_moulton(float(string_parts[1]), float(string_parts[-5]), float(string_parts[-4]),
+			int(string_parts[-3]), string_parts[-2], int(string_parts[-1]), y_list, saida)
+		saida.write("\n")
+	else: 
+		saida.write("Metodo nao reconhecido\n\n")
 entrada.close()
 saida.close()
