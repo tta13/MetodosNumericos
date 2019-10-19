7d279947e3760c15f1adaa762b5c0349f50fc40c
tta13 <54077178+tta13@users.noreply.github.com>
2019-10-19 17:10:05 -0300

    Adicionando package para gerar graficos

---
+++ graficos.py
@@ -0,0 +1,16 @@
+#Plotar graficos dos metodos
+import matplotlib.pyplot as plt
+
+def plotar(t0, h, n, y, titulo):
+	t = []
+	t0 = float(t0)
+	h = float(h)
+	n = int(n)
+	for i in range(0, n+1):
+		t.append(t0)
+		t0 += h
+	plt.plot(t, y) 
+	plt.xlabel('t') 
+	plt.ylabel('y') 
+	plt.title(titulo) 
+	plt.show() 
